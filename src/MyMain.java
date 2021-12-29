bc49c8c9d7c966297d2dcbb486a3c7964e30507a
mohamedhassan218 <m.hassan.def@gmail.com>
2021-12-29 21:22:41 +0200

    Add, remove and edit

--- src/MyMain.java
+++ src/MyMain.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 
 public class MyMain 
 {
-	
-	
-	
 	public static void main(String[] args) 
 	{
-		ArrayList<Person> persons = new ArrayList<Person>();
+		
+		ArrayList<Student> students = new ArrayList<Student>();
+		ArrayList<Adminnistrator> adminnistrators = new ArrayList<Adminnistrator>();
+		ArrayList<Instructor> instructors = new ArrayList<Instructor>();
+		ArrayList<Worker> workers = new ArrayList<Worker>();
+		ArrayList<Technical> technicals = new ArrayList<Technical>();
 
 		char personType;
 		char taskType;
 		boolean whileKey = true;
+		int whileKeyInt;
+		
 		//student attributes
 		String studentName;
 		String studentNationalNumber;
@@ -27,16 +31,20 @@ public class MyMain
 		
 		
 		while(whileKey) 
-		{
+		{// start of while loop
+			
 			System.out.println("Hi user, please select a number!");
 			System.out.println("1 ---> Student");
 			System.out.println("2 --> Instructor");
 			System.out.println("3 --> Technician");
 			System.out.println("4 --> Worker");
 			System.out.println("5 --> Administrator");
+			
 			personType = input.next().charAt(0);
-			if(personType == '1')
-			{
+			
+			if(personType == '1')                         //Student
+			{// start of student
+				
 				System.out.println("To add --> 1");
 				System.out.println("To remove --> 2");
 				System.out.println("To show information --> 3");
@@ -46,62 +54,102 @@ public class MyMain
 				taskType = input.next().charAt(0);
 				
 				switch(taskType)
-				{
+				{// start of student tasks
 				
 				//adding
 				case '1':
-					System.out.print("The name: ");
-					int option = input.nextInt();
-					input.nextLine();
-					studentName = input.nextLine();
-					System.out.println();
-					System.out.print("The national number: ");
-					int option2 = input.nextInt();
-					input.nextLine();
-					studentNationalNumber = input.nextLine();
-					System.out.println();
-					System.out.print("The age: ");
-					studentAge = input.nextInt();
-					System.out.println();
-					System.out.print("The address: ");
-					int option3 = input.nextInt();
-					input.nextLine();
-					studentAddress = input.nextLine();
-					System.out.println();
-					System.out.print("The gpa: ");
-					studentGpa = input.nextDouble();
-					System.out.println();
-					System.out.print("The acadimic number: ");
-					int option4 = input.nextInt();
-					input.nextLine();
-					studentAcadimicNumber = input.nextLine();
-					System.out.println();
-					System.out.print("The acadimic mail: ");
-					int option5 = input.nextInt();
-					input.nextLine();
-					studentAcadimicMail = input.nextLine();
-					System.out.println();
-					Student s1 = new Student(studentName, studentNationalNumber, studentAcadimicNumber, studentAcadimicMail);
-					s1.setAddress(studentAddress);
-					s1.setAge(studentAge);
-					s1.setGpa(studentGpa);
-					persons.add(s1);
+					try 
+					{
+						System.out.print("The name: ");
+						input.nextLine();
+						studentName = input.nextLine();
+						System.out.print("The national number: ");
+						studentNationalNumber = input.nextLine();
+						System.out.print("The age: ");
+						studentAge = input.nextInt();
+						System.out.print("The address: ");
+						input.nextLine();
+						studentAddress = input.nextLine();
+						System.out.print("The gpa: ");
+						studentGpa = input.nextDouble();
+						System.out.print("The acadimic number: ");
+						input.nextLine();
+						studentAcadimicNumber = input.nextLine();
+						System.out.print("The acadimic mail: ");
+						studentAcadimicMail = input.nextLine();
+						Student s1 = new Student(studentName, studentNationalNumber, studentAcadimicNumber, studentAcadimicMail);
+						s1.setAddress(studentAddress);
+						s1.setAge(studentAge);
+						s1.setGpa(studentGpa);
+						students.add(s1);
+
+						System.out.println(s1);                 // test the information
+					
+					}
+					catch(Exception e)
+					{
+						System.out.println("Invalid Data");
+					}
+					
+					System.out.println("If you want to exit enter '0' else enter any char!");
+					whileKeyInt = input.nextInt();
+					if(whileKeyInt == 0)
+					{
+						whileKey = false;
+					}
 					break;
 				
 				//removing	
 				case '2':
+					try
+					{
+						System.out.println("Enter the national number of the student who you want to remove: ");
+						String sNationalNumber = input.next();
+						for(int i =0; i < students.size(); i++)
+						{
+							Student x = students.get(i);
+							if(x.getNationalNumber() == sNationalNumber)
+							{
+								students.remove(x);
+								break;
+							}
+						}
+						System.out.println("Student information has been removed. ");
+					}
+					catch(Exception e)
+					{
+						System.out.println("Wrong Data");
+					}
+					
+				
+				//Show all students
+				case '3': 
+					try
+					{
+						for(int i =0; i < students.size(); i++)
+						{
+							Student x = students.get(i);
+							System.out.println(x);
+						}
+					}
+					catch(Exception e)
+					{
+						System.out.println("Wrong data!");
+					}
+					
+					
 					
 					
 					
 					
 
 							
-				   }
-			    }
+				   }// end of student tasks
+			    
+			}//end of student
 		
-		
-		}
-		
-	}
+		}// end of while loop	
 
-}
+	}// end of main
+
+}// end of class
